3fe4a605bd2eb0c020e82b45bd86f0ae447f4f90
Daud Hoshen Rimon <daudhrimon@gmail.com>
2022-04-07 21:59:13 +0600

    first

---
+++ app/src/main/java/com/daud/calculator/MainActivity.java
@@ -0,0 +1,84 @@
+package com.daud.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void errorBtnClick(View view) {
+    }
+
+    public void pieBtnClick(View view) {
+    }
+
+    public void sinBtnClick(View view) {
+    }
+
+    public void degBtnClick(View view) {
+
+    }
+
+    public void clearBtnClick(View view) {
+    }
+
+    public void bStartBtnClick(View view) {
+    }
+
+    public void divideBtnClick(View view) {
+    }
+
+    public void sevenBtnClick(View view) {
+    }
+
+    public void eightBtnClick(View view) {
+    }
+
+    public void nineBtnClick(View view) {
+    }
+
+    public void intoBtnClick(View view) {
+    }
+
+    public void fourBtnClick(View view) {
+    }
+
+    public void fiveBtnClick(View view) {
+    }
+
+    public void sixBtnClick(View view) {
+    }
+
+    public void minusBtnClick(View view) {
+    }
+
+    public void oneBtnClick(View view) {
+    }
+
+    public void twoBtnClick(View view) {
+    }
+
+    public void threeBtnClick(View view) {
+    }
+
+    public void plusBtnClick(View view) {
+    }
+
+    public void zeroBtnClick(View view) {
+    }
+
+    public void pointBtnClick(View view) {
+    }
+
+    public void equalBtnClick(View view) {
+    }
+}
